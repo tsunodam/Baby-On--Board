76fa2b5959aff57c97cf41fd5237829b9d4e6e32
tsunodam <tsunodam@bu.edu>
2020-02-01 16:34:25 -0500

    Navigation Bar edited with 4 items

--- app/src/main/java/com/example/babyonboard/Home.java
+++ app/src/main/java/com/example/babyonboard/Home.java
@@ -15,12 +15,12 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
+        setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_diary, R.id.navigation_care, R.id.navigation_resources)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
