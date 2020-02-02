bb779be088ab2c577f0f357b65d6b0ec1c35fd0e
poojachainani13 <pooja13@bu.edu>
2020-02-01 20:26:27 -0500

    fixed calendar, added comments for working the calendar

--- app/src/main/java/com/example/babyonboard/ui/diary/DiaryFragment.java
+++ app/src/main/java/com/example/babyonboard/ui/diary/DiaryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,18 +19,20 @@ public class DiaryFragment extends Fragment {
 
     private DiaryViewModel diaryViewModel;
 
+    //private array
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         diaryViewModel =
                 ViewModelProviders.of(this).get(DiaryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_diary, container, false);
-        final TextView textView = root.findViewById(R.id.text_diary);
-        diaryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final CalendarView calendarView = root.findViewById(R.id.calendarView);
+
+        // function for add to calendar
+            // add to array
+        // funtion for view
+            // read array
+
         return root;
     }
 }
